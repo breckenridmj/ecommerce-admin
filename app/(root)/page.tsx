7babc309e762c53ebf54418c23541e2722314749
Malcolm Breckenridge <breckenridmj@vcu.edu>
2023-07-29 11:02:54 -0500

    created middleware.ts file

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
     return (
-      <p>Hello Admin Dashboard</p>
+        <div className="p-4">
+            <Button size="default">Click Me</Button>
+        </div>
     )
   }
   
