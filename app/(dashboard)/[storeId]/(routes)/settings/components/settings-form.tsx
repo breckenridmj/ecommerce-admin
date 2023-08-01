8b00384f1c3473b467dcc8be5906f3bb59d6f3f7
Malcolm Breckenridge <breckenridmj@vcu.edu>
2023-08-01 14:47:28 -0500

    updated settings-form.tsx npx shadcn-ui@latest add separator

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,9 +1,33 @@
 "use client"
 
-export const SettingsForm = () => {
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+
+interface SettingsFormsProps {
+    initialData: Store;
+}
+
+export const SettingsForm: React.FC<SettingsFormsProps> = ({
+    initialData
+}) => {
     return (
-        <div>
-            Settings Form
-        </div>
+        <>
+            <div className="flex items-center justify-between">
+                <Heading 
+                    title="Settings"
+                    description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => {}}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+        </>
     );
 };
